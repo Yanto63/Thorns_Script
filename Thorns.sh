69c6942433b8a276f85066ea04903c42fd5311a3
yann.champeau <yann.champeau@codefirst.iut.uca.fr>
2023-10-11 17:50:29 +0200

    Engaging:gear:

---
+++ Thorns.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+nbUsr=1
+
+while [ $nbUsr -eq 1 ]; do
+	nbUsr=$(who | wc -l)
+	sleep 2
+done
+
+target=$(who | grep -v yachampeau | cut -d '(' -f 2 | cut -d ')' -f 1)
+echo $target
+echo "yes" | ssh $target
